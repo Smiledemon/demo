bcd1062756bf37377114345517e490adb6b1790f
Smiledemon <790333234@qq.com>
2018-01-15 16:10:40 +0800

    输出hello world

---
+++ src/main/java/com/xceder/Test.java
@@ -0,0 +1,15 @@
+package com.xceder;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ *
+ * @author: Mac
+ * Date: 2018-01-15
+ * Time: 下午4:09
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
