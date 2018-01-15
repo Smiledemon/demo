7b651d09776e655b2986734829db75dd31e78fad
Smiledemon <790333234@qq.com>
2018-01-15 15:50:16 +0800

    输出0-99的数

---
+++ src/main/java/com/xceder/Number.java
@@ -0,0 +1,17 @@
+package com.xceder;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ *
+ * @author: Mac
+ * Date: 2018-01-15
+ * Time: 下午3:49
+ */
+public class Number {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i);
+        }
+    }
+}
