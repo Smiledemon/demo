4c682d6d1389a2deb8d3a7b560c5ef0f690cad7c
Smiledemon <790333234@qq.com>
2018-01-15 11:55:30 +0800

    创建基础环境

---
+++ src/main/java/com/xceder/Demo.java
@@ -0,0 +1,15 @@
+package com.xceder;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ *
+ * @author: Mac
+ * Date: 2018-01-15
+ * Time: 上午11:54
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Hello github!");
+    }
+}
