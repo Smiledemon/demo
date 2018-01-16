765d797bc89b8f55bafc2a2c54befe6330a51cd9
Smiledemon <790333234@qq.com>
2018-01-16 09:58:45 +0800

    异常

---
+++ src/main/java/com/xceder/Error.java
@@ -0,0 +1,18 @@
+package com.xceder;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ *
+ * @author: Mac
+ * Date: 2018-01-16
+ * Time: 上午9:56
+ */
+public class Error {
+    public static void main(String[] args) {
+        int[] array=new int[5];
+        for (int i = 0; i <=5; i++) {
+            array[i]=1;
+        }
+    }
+}
