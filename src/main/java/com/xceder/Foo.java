ae006d4dedd03418343f83a0369558f1a367784f
Smiledemon <790333234@qq.com>
2018-01-18 14:47:47 +0800

    Hello

---
+++ src/main/java/com/xceder/Foo.java
@@ -0,0 +1,15 @@
+package com.xceder;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ *
+ * @author: Mac
+ * Date: 2018-01-18
+ * Time: 下午2:47
+ */
+public class Foo {
+    public static void main(String[] args) {
+        System.out.println("Hello,man!");
+    }
+}
