afa0beede3e625665e77aaf5738bf55d6d03a945
Smiledemon <790333234@qq.com>
2018-01-18 15:30:57 +0800

    输出

---
+++ src/main/java/com/xceder/Hsdf.java
@@ -0,0 +1,15 @@
+package com.xceder;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ *
+ * @author: Mac
+ * Date: 2018-01-18
+ * Time: 下午3:30
+ */
+public class Hsdf {
+    public static void main(String[] args) {
+        System.out.println("1234456");
+    }
+}
