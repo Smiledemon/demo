9c50d33d1ae163b2e30e1b6e7646deaa24ba7ae1
Smiledemon <790333234@qq.com>
2018-01-22 16:06:50 +0800

    输出

---
+++ src/main/java/com/xceder/FunctionFor.java
@@ -0,0 +1,15 @@
+package com.xceder;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ *
+ * @author: Mac
+ * Date: 2018-01-22
+ * Time: 下午4:06
+ */
+public class FunctionFor {
+    public static void main(String[] args) {
+        System.out.println("Junit");
+    }
+}
