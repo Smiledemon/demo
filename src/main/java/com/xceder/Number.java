d126a8f7ada6f4a8141f297fa07ab33047208892
Smiledemon <34064939+Smiledemon@users.noreply.github.com>
2018-01-22 17:34:34 +0800

    Revert "Update"

--- src/main/java/com/xceder/Number.java
+++ src/main/java/com/xceder/Number.java
@@ -11,7 +11,7 @@ package com.xceder;
 public class Number {
     public static void main(String[] args) {
         for (int i = 0; i < 100; i++) {
-            System.out.println(i+1);
+            System.out.println(i);
         }
     }
 }
