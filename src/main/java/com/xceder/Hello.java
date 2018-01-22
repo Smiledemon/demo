36dee793dbc1ced12ba2220ca7921780b3725d76
Smiledemon <34064939+Smiledemon@users.noreply.github.com>
2018-01-22 17:42:41 +0800

    Update Hello.java

--- src/main/java/com/xceder/Hello.java
+++ src/main/java/com/xceder/Hello.java
@@ -11,5 +11,7 @@ package com.xceder;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hellosdfsdfsdfasfd");
+        System.out.println("Hellosdfsdfsdfasfd");
+        System.out.println("Hellosdfsdfsdfasfd");
     }
 }
