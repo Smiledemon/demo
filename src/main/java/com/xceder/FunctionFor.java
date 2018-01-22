76ea3a1828fee95124a416f5cc63a7aecd379bf1
Smiledemon <34064939+Smiledemon@users.noreply.github.com>
2018-01-22 17:44:28 +0800

    Update FunctionFor.java

--- src/main/java/com/xceder/FunctionFor.java
+++ src/main/java/com/xceder/FunctionFor.java
@@ -9,7 +9,7 @@ package com.xceder;
  * Time: 下午4:06
  */
 public class FunctionFor {
-    public static void main(String[] args) {
+    public static void main(String args[]) {
         System.out.println("Junit");
     }
 }
