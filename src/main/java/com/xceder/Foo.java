7343822b3a18d5f8082dce9c832bb7323399594e
Smiledemon <790333234@qq.com>
2018-02-28 14:46:40 +0800

    hello

--- src/main/java/com/xceder/Foo.java
+++ src/main/java/com/xceder/Foo.java
@@ -11,6 +11,6 @@ package com.xceder;
 public class Foo {
     public static void main(String[] args) {
         System.out.println("Hello,woman!");
-        System.out.println("Hello,woman!");
+        System.out.println("Hello,woman!");System.out.println("Hello,woman!");
     }
 }
